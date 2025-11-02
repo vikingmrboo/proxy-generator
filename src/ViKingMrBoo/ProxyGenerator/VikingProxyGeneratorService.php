833874649b057cd652d1cc207b9b1719f0d22d0a
Vladimir Kushnarev <vladimir.kushnaryov@megafon.ru>
2025-11-02 12:28:49 +0300

    still not working

--- src/ViKingMrBoo/ProxyGenerator/VikingProxyGeneratorService.php
+++ src/ViKingMrBoo/ProxyGenerator/VikingProxyGeneratorService.php
@@ -12,8 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class VikingProxyGeneratorService
 {
+    /**
+     * @var ProxyGenerator
+     */
     private $proxyGenerator;
+
+    /**
+     * @var AnnotationReader
+     */
     private $annotationReader;
+
+    /**
+     * @var CacheProvider
+     */
     private $cache;
 
     public function __construct(ProxyGenerator $proxyGenerator, AnnotationReader $annotationReader, CacheProvider $cache)
@@ -23,6 +34,18 @@ class VikingProxyGeneratorService
         $this->cache = $cache;
     }
 
+    private static function getUsages(ReflectionClass $reflectionClass): string
+    {
+        if(!$content = file_get_contents($reflectionClass->getFileName())) {
+            throw new \RuntimeException("Unable to read file {$reflectionClass->getFileName()}");
+        }
+
+        $pattern = '/use\s+(?:function|const\s+)?\S+(?:\s+as\s+\S+\s+)?;/m';
+        preg_match_all($pattern, $content, $matches, PREG_SET_ORDER, 0);
+
+        return implode("\n", $matches[0]);
+    }
+
     public function generateProxyClass($interfaceName, $config)
     {
         $cacheKey = 'proxy_' . $interfaceName;
@@ -30,20 +53,22 @@ class VikingProxyGeneratorService
             return $this->cache->fetch($cacheKey);
         }
 
+
         $reflectionClass = new \ReflectionClass($interfaceName);
+        $usages = self::getUsages($reflectionClass);
         $methods = $reflectionClass->getMethods();
 
         $proxyClassName = $interfaceName . 'Proxy';
-        $proxyClassCode = $this->generateProxyClassCode($proxyClassName, $methods, $config);
+        $proxyClassCode = $this->generateProxyClassCode($proxyClassName, $usages, $methods, $config);
 
-        file_put_contents($this->proxyGenerator->getProxyNamespace() . '/' . $proxyClassName . '.php', $proxyClassCode);
+        file_put_contents($this->proxyGenerator->getProxyFileName($proxyClassName), $proxyClassCode);
 
         $this->cache->save($cacheKey, $proxyClassName);
 
         return $proxyClassName;
     }
 
-    private function generateProxyClassCode($proxyClassName, $methods, $config)
+    private function generateProxyClassCode($proxyClassName, $usages, $methods, $config)
     {
         $methodsCode = '';
         foreach ($methods as $method) {
@@ -57,6 +82,7 @@ namespace {$this->proxyGenerator->getProxyNamespace()};
 
 use {$this->proxyGenerator->getProxyNamespace()}\\AbstractClient;
 use {$method->getDeclaringClass()->getNamespaceName()}\\{$method->getDeclaringClass()->getShortName()};
+{$usages}
 
 class $proxyClassName extends AbstractClient implements {$method->getDeclaringClass()->getShortName()}
 {
@@ -73,60 +99,76 @@ PHP;
         }
 
         $path = $routeAnnotation->getPath();
-        $methods = $routeAnnotation->getMethods();
+        $requestMethod = $routeAnnotation->getMethods()[0];
         $name = $routeAnnotation->getName();
         $options = $routeAnnotation->getOptions();
         $parametersMap = [];
 
         foreach ($method->getParameters() as $parameter) {
-            if (!$type = $parameter->getType()) {
-                $type = 'string';
-            } else {
-                $type = $type instanceof \ReflectionNamedType ? $type->getName() : (string) $type;
-            }
-
             $parametersMap[$parameter->getName()] = $parameter;
         }
 
-        $arguments = '';
-        $delimiter = '?';
-
-        if (preg_match_all('/(?:([a-zA-Z0-9_-]+)=)?\{([A-Za-z0-9_]+)}/', $path, $matches)) {
-            foreach ($matches[2] as $key => $match) {
+        if (preg_match_all('/\{([A-Za-z0-9_]+)}/', $path, $matches)) {
+            foreach ($matches[1] as $key => $match) {
                 if (!isset($parametersMap[$match])) {
                     continue;
                 }
 
                 $parameter = $parametersMap[$match];
+                $type = self::detectType($parameter) ?: 'string';
 
-                if (class_exists($parameter->getType())) {
-                    $parameterMetadata = new \ReflectionClass($parameter->getType());
+                if (class_exists($type)) {
+                    $classMetadata = new \ReflectionClass($type);
+                    $arguments = '';
+                    $delimiter = '';
 
-                    foreach ($parameterMetadata->getProperties() as $property) {
-                        if (!empty($matches[1][$key])) {
+                    foreach ($classMetadata->getProperties() as $property) {
+                        $propertyName = "{\${$property->getName()}}";
 
+                        if (!$property->isPublic()) {
+                            $upCased = ucfirst($property->getName());
+
+                            if ($classMetadata->hasMethod("get{$upCased}")) {
+                                $propertyName = "{\${$parameter->getName()}->get{$upCased}()}";
+                            } elseif ($classMetadata->hasMethod("is{$upCased}")) {
+                                $propertyName = "{\${$parameter->getName()}->is{$upCased}()}";
+                            } else {
+                                continue;
+                            }
                         }
+
+                        $arguments .= "{$delimiter}{$property->getName()}={$propertyName}";
+                        $delimiter = '&,';
                     }
+
+                    $path = str_replace($matches[0][$key], $arguments, $path);
+                } else {
+                    $path = str_replace($matches[0][$key], "\${$parameter->getName()}", $path);
                 }
             }
         }
 
         $timeout = isset($options['timeout']) ? $options['timeout'] : $config['timeout'];
 
-        $parametersCode = '';
-        foreach ($method->getParameters() as $parameter) {
-            $parametersCode .= '$' . $parameter->getName() . ', ';
-        }
-        $parametersCode = rtrim($parametersCode, ', ');
-
-        if (in_array('POST', $methods)) {
-            $bodyCode = '$body = $' . $method->getParameters()[0]->getName();
+        if (!empty($parametersMap) && in_array($requestMethod, ['POST', 'PUT', 'UPDATE'])) {
+            $parameter = end($parametersMap);
+            $bodyCode = "\${$parameter->getName()}";
+        } else {
+            $bodyCode = 'null';
         }
 
-        $responseCode = 'return $this->request("' . implode('|', $methods) . '", "' . $config['url'] . $path . '", ' . ($bodyCode ? '$body' : 'null') . ', ["timeout" => "' . $timeout . '", "name" => "' . $name . '"]);';
+        $responseCode = "return \$this->request('{$requestMethod}', '{$config['url']}{$path}', $bodyCode, ['name' => '{$name}', 'timeout' => '{$timeout}']);";
 
         $returnType = $method->getReturnType();
         $returnTypeCode = $returnType ? $returnType->getName() : 'mixed';
+
+        $parametersCode = implode(', ', array_map(function (\ReflectionParameter $parameter): string {
+            if($type = self::detectType($parameter)) {
+                $type = " {$type}";
+            }
+
+            return "{$type}{$parameter->getName()}";
+        }, $parametersMap));
 
         return <<<PHP
 public function {$method->getName()}($parametersCode): $returnTypeCode
@@ -144,5 +186,28 @@ PHP;
         }
 
         return "\${$property->getName()},";
+    }
+
+    private static function detectType(\ReflectionParameter $parameter): ?string
+    {
+        $type = $parameter->getType();
+
+        if (!$type) {
+            return null;
+        }
+
+        if ($type instanceof \ReflectionNamedType) {
+            return $type->getName();
+        }
+
+        if ($type instanceof \ReflectionUnionType) {
+            // Обработка объединенных типов (PHP 8+)
+            $types = array_map(function($type) {
+                return $type->getName();
+            }, $type->getTypes());
+            return implode('|', $types);
+        }
+
+        return (string) $type;
     }
 }
